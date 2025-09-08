1232875f498dd8a2fab29c0d1c03e3de7c486d50
eozeneto <josetorresnetoes@gmail.com>
2025-09-08 14:25:43 -0300

    Create Processo.java
    Classe Processo

---
+++ Processo.java
@@ -0,0 +1,15 @@
+public class Processo{
+    int id;
+    String nome;
+    int prioridade;
+    int ciclosNecessarios;
+    String recursosNecessarios;
+
+    public Processo (int id, String nome, int prioridade, int ciclosNecessarios, String recursosNecessarios){
+    this.id = id;
+    this.nome = nome;
+    this.prioridade = prioridade;
+    this.ciclosNecessarios = ciclosNecessarios;
+    this.recursosNecessarios = recursosNecessarios;}
+
+}
