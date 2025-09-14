5d6aefc2f5698a6e0337a30b73baef77289cd13a
DevTavynho <octavioalvesfla@gmail.com>
2025-09-14 14:43:43 -0300

    add node duplo

--- Node.java
+++
@@ -1,9 +0,0 @@
-public class Node {
-    Processo processo;
-    Node next;
-    
-    public Node(Processo processo) {
-        this.processo = processo;
-        this.next = null;
-    }
-}
---
+++ NodeDuplo.java
@@ -0,0 +1,12 @@
+public class NodeDuplo {
+    Processo processo;
+    NodeDuplo next;
+    NodeDuplo before;
+
+    public NodeDuplo(Processo processo) {
+        this.processo = processo;
+        this.next = null;
+        this.before = null;
+    }
+}
+NodeDuplo.java
