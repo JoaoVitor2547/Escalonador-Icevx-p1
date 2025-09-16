d63dce78dc606ae7d3d59e1551796d75277917ca
JoaoVitor2547 <joaovitorf1611@gmail.com>
2025-09-16 10:44:42 -0300

    Update Processo.java
    Adicionei uma int prioridadeOriginal, para guardar a fila de origem e organizei os this.

--- Processo.java
+++ Processo.java
@@ -1,20 +1,23 @@
 public class Processo{
     int id;
     String nome;
-    int prioridade;
+    int prioridade; // 1 = Alta, 2 = Média, 3 = Baixa
     int ciclosNecessarios;
     String recursosNecessarios;
     public boolean jaBloqueado;
+    int prioridadeOriginal; 
 
-    public Processo (int id, String nome, int prioridade, int ciclosNecessarios, String recursosNecessarios, boolean jaBloqueado){
-    this.id = id;
-    this.nome = nome;
-    this.prioridade = prioridade;
-    this.ciclosNecessarios = ciclosNecessarios;
-    this.recursosNecessarios = recursosNecessarios;
-    this.jaBloqueado = jaBloqueado;
-}
-@Override
+    public Processo(int id, String nome, int prioridade, int ciclosNecessarios, String recursosNecessarios, boolean jaBloqueado) {
+        this.id = id;
+        this.nome = nome;
+        this.prioridade = prioridade;
+        this.ciclosNecessarios = ciclosNecessarios;
+        this.recursosNecessarios = recursosNecessarios;
+        this.jaBloqueado = jaBloqueado;
+        this.prioridadeOriginal = prioridade; 
+    }
+
+    @Override
     public String toString() {
         return "[P" + id + " - " + nome + " | Prioridade: " + prioridade + " | Ciclos: " + ciclosNecessarios + "]";
     }
